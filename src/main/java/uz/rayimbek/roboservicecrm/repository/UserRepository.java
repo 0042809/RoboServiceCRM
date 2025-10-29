530545e5bda172aed462dd1aa93e8175cfe49bf0
Rayimbek <114511835+0042809@users.noreply.github.com>
2025-10-29 08:59:50 +0500

    UserRepository updated

--- src/main/java/uz/rayimbek/roboservicecrm/repository/UserRepository.java
+++ src/main/java/uz/rayimbek/roboservicecrm/repository/UserRepository.java
@@ -3,5 +3,8 @@ package uz.rayimbek.roboservicecrm.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import uz.rayimbek.roboservicecrm.entity.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String userName);
 }
